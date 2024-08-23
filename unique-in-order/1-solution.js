28a995147c6ce701beef156b4dd06a3462b15f7b
Ervin Mamutov <ervin.news@gmail.com>
2024-08-23 17:41:14 +0200

    new task

--- unique-in-order/1-solution.js
+++ unique-in-order/1-solution.js
@@ -2,6 +2,7 @@
 
 const uniqueInOrder = (iterable) => {
   //your code here - remember iterable can be a string or an array
+  iterable.map((char) => charCodeAt(char));
 };
 
 require('./test.js')(uniqueInOrder);
